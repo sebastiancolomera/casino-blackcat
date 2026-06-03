38aacf85a81195af3ed8ff67fd81fbd557e69959
sebastiancolomera <s.colomera01@ufromail.cl>
2026-06-03 01:04:43 -0400

    refactor(modelo): implementar Serializable en Resultado

--- src/main/java/modelo/Resultado.java
+++ src/main/java/modelo/Resultado.java
@@ -1,6 +1,9 @@
 package modelo;
 
-public class Resultado {
+import java.io.Serializable;
+
+public class Resultado implements Serializable {
+    private static final long serialVersionUID = 1L;
     private final int numero;
     private final int apuesta;
     private final boolean acierto;
