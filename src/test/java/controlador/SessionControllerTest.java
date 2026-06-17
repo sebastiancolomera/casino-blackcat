e5542721ab4437f6a0bb202bd0fcf092be7b8974
sebastiancolomera <s.colomera01@ufromail.cl>
2026-06-16 21:59:36 -0400

    test(session): pruebas login con username nulo o password nulo son rechazados

--- src/test/java/controlador/SessionControllerTest.java
+++ src/test/java/controlador/SessionControllerTest.java
@@ -13,4 +13,21 @@ class SessionControllerTest {
         Usuario resultado = session.login("usuarioInexistente", "contraseña");
         assertNull(resultado);
     }
+
+    @Test
+    void loginConUsernameNuloEsRechazado() {
+        SessionController session = new SessionController();
+
+        Usuario resultado = session.login(null, "contraseña");
+
+        assertNull(resultado);
+    }
+    @Test
+    void loginConPasswordNuloEsRechazado() {
+        SessionController session = new SessionController();
+
+        Usuario resultado = session.login("TimPayne", null);
+
+        assertNull(resultado);
+    }
 }
